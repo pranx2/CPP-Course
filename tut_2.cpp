0e4686e9217001cafc662d4b5996a2a0118a3526
PRAN <prantokumar.net@gmail.com>
2024-04-03 22:21:26 +0600

    new

--- tut_2.cpp
+++ tut_2.cpp
@@ -9,6 +9,8 @@ using namespace std;
    char c; // characters;
       a = 7 ;
       b = 8 ;
+      double d; //big float numbers;
+      bool d; // true and false; 0,1;
       cout<<"Total sum is: "<<a+b; 
    return 0;
  }
---
+++ tut_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int digit = 10; //Global variables;
+void sol(){
+   cout<<digit<<"\n";
+}
+
+int main(){
+   int a = 10;//local variables;
+   int b = 12;
+   sol();
+   cout<<"a= "<<a <<"\nb= "<< b;
+}
