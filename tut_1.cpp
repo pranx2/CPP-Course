d636dba94ea3ed3d1708580d6eba2ea4d219871f
PRAN <prantokumar.net@gmail.com>
2024-04-03 12:35:34 +0600

    new

---
+++ tut_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"Hello World!";// This line outputs the string "Hello World!" to the standard output
+return 0;// This line indicates that the program executed successfully and returns 0 to the operating system
+
+}
+//  Using iostream for programming.... 
+// it means it has input/output.
+// int means the main will give integers value...
+// Std means STANDARD.[ std:: ] or [ using namespace std; ]
+// Cout means C--OUT...
