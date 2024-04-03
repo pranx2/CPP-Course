5953d305f25927ca5ea439a518bcd832582e7347
PRAN <prantokumar.net@gmail.com>
2024-04-03 13:19:13 +0600

    new

---
+++ tut_2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+ int main(){
+   // Variables;
+   //_____________
+   int a; //Integers;
+   float b; // float;
+   char c; // characters;
+      a = 7 ;
+      b = 8 ;
+      cout<<"Total sum is: "<<a+b; 
+   return 0;
+ }
