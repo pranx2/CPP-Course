e3fef21c08dccb30ca31cd5351456112df3e76c4
PRAN <prantokumar.net@gmail.com>
2024-04-04 20:33:02 +0600

    new

--- tut_5.cpp
+++ tut_5.cpp
@@ -1,6 +1,39 @@
 #include<iostream>
 using namespace std;
 int main(){
-   
+   cout<<"I AM PRANTO"<<endl;
+   cout<<"I AM Pritom"<<endl;
+
+   int a=20, b =30;
+
+   // Arithmetic operators;
+   cout<<"The value of a+b is : "<<a+b<<endl;
+   cout<<"The value of a-b is : "<<a-b<<endl;
+   cout<<"The value of a/b is : "<<a/b<<endl;
+   cout<<"The value of a%b is : "<<a%b<<endl;
+   cout<<"The value of a++ is : "<<a++<<endl; //Post increments;
+   cout<<"The value of a-- is : "<<a--<<endl;//post decrements;
+   cout<<"The value of ++a is : "<<++a<<endl;//pre increments;
+   cout<<"The value of --a is : "<<--a<<endl;//pre decrements;
+   cout<<endl;
+
+   // Assignment operators;
+      // int a = 9;
+      // int b = 10;
+      char c = 'c';
+
+   // comparison operators;
+      cout<<"comparison / logical operators"<<endl;
+      cout<<endl;
+
+      cout<<"a = 9 , b = 10. a==b : "<<(a==b)<<endl;   
+      cout<<"a = 9 , b = 10. a>=b : "<<(a>=b)<<endl;   
+      cout<<"a = 9 , b = 10. a<=b : "<<(a<=b)<<endl;   
+      cout<<"a = 9 , b = 10. a>b : "<<(a>b)<<endl;   
+      cout<<"a = 9 , b = 10. a<b : "<<(a<b)<<endl;   
+      cout<<"a = 9 , b = 10. a!=b : "<<(a!=b)<<endl;   
+      cout<<"a = 9 , b = 10. (a!=b) && (a>=b) : "<<((a!=b) && (a>=b))<<endl;   
+      cout<<"a = 9 , b = 10. (a!=b) || (a>=b) : "<<((a!=b) || (a<=b))<<endl;   
+      cout<<"a = 9 , b = 10. !(a=!b) : "<<(!(a=!b))<<endl;   
    return 0;
 }
---
+++ tut_6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+   using namespace std;
+int c = 79; //Global variables;
+
+int main(){
+   int a,b,c;
+      cout<<"Enter the a variables data : "<<endl;
+      cin>>a;
+      cout<<"Enter the a variables data : "<<endl;
+      cin>>b;
+      c = a+b;
+      cout<<"Enter the a variables data : "<<c<<endl;
+
+      cout<<"Global variables : "<<::c<<endl; // printing global variables;
+   // Typecasting;
+   float x = 60.99;
+   cout<<"the float value of int is : "<<(int)x<<endl;
+   return 0;
+}
