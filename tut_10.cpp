4baa1a8e4de402989330d16ff15cbe57dd31290b
PRAN <prantokumar.net@gmail.com>
2024-04-06 21:05:53 +0600

    new

--- tut_10.cpp
+++ tut_10.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 int main(){
    //For loop in c++;
-   // while, Do while;
-            for(int i = 0; i < 4; i++){
-               cout<<i<<endl;
-            }
+   // for(int i = 0; i < 4; i++){
+   //    cout<<i<<endl;
+   // }
+   // // while, Do while;
+   // cout<<"Printing While loops->"<<endl;
+   // int i = 0;
+   // while( i <= 50){
+   //    cout<<i<<endl;
+   //    i++;
+   // }
+
+   // Do while loops ;
+   cout<<"Do while loops;"<<endl;
+   int i = 1;
+   do{
+      cout<<6*i<<endl;
+      i++;
+   } while ( i <= 10);
 return 0;
 }
