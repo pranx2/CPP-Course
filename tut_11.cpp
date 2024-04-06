591ab0dc87fb964031e0459984973010c5dd3586
PRAN <prantokumar.net@gmail.com>
2024-04-06 22:15:47 +0600

    new

--- tut_11.cpp
+++ tut_11.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(){
    for(int i = 1; i < 10; i++){
-      cout<<i<<endl;
       if (i == 2){
-         break;
+         // break;
+         continue;
       }
+      cout<<i<<endl;
    }
 return 0;
 }
