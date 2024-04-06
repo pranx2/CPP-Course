6415fe8b015ebfdd69aa3279c4d4dfa5c7bdc19f
PRAN <prantokumar.net@gmail.com>
2024-04-06 20:48:45 +0600

    new

---
+++ tut_10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   //For loop in c++;
+   // while, Do while;
+            for(int i = 0; i < 4; i++){
+               cout<<i<<endl;
+            }
+return 0;
+}
---
+++ tut_9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age;
+   cin>>age;
+   switch (age)
+   {
+   case 18:
+   cout<<"Your are 18."<<endl;
+      break;
+   
+   case 22:
+   cout<<"The are 18+."<<endl;
+      break;
+
+   default:
+   cout<<"Not special cases."<<endl;
+      break;
+   }
+return 0;
+}
