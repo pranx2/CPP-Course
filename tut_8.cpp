88a8ba4af2553aecf1a85d3c44a0b4c3bb32fdce
PRAN <prantokumar.net@gmail.com>
2024-04-06 08:25:13 +0600

    new

--- tut_8.cpp
+++ tut_8.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
+using namespace std;
 int main(){
-   
+   //If-Else case;
+   int a = 50, b = 60;
+   int age;
+   cout<<"Tell me your age: ";
+   cin>>age;
+      
+      if(age < 18){
+         cout<<"You cannot come to the party :)"<<endl;
+      }
+      else if( age == 18){
+         cout<<"welcome to the party!"<<endl;
+      }
+      else cout<<"You are welcome to the party!"<<endl;
 return 0;
 }
