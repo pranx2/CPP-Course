dcbb51f3342400d31beed58db14fb130424394e9
PRAN <prantokumar.net@gmail.com>
2024-04-06 08:13:22 +0600

    new

--- tut_7.cpp
+++ tut_7.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-      const int a = 4; // fixed number ;cannot be change;
+      const int a = 005; // fixed number ;cannot be change;
       cout<<"The value of a is : "<<a;
    return 0;
 }
