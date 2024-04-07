6e9c2d3f39c129478207c7c9d6e43f494f5b46fb
PRAN <prantokumar.net@gmail.com>
2024-04-07 20:58:21 +0600

    pointer

--- tut_12.cpp
+++ tut_12.cpp
@@ -6,5 +6,11 @@ int main(){
    cout<<"The address of a is: "<<&a<<endl;
    cout<<"The address of a in b: "<<b<<endl;
    cout<<"The value of b is: "<<*b<<endl;
+//  pointer to pointer
+int** c = &b;
+cout<<"The address of b is : "<<&b<<endl;
+cout<<"The address of b in c is : "<<c<<endl;
+cout<<"The value of b in c is : "<<**c<<endl;
+
 return 0;
 }
