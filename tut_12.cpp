0428b028b003224a4060f8ada87aa669970e03a2
PRAN <prantokumar.net@gmail.com>
2024-04-07 20:51:57 +0600

    new

--- tut_12.cpp
+++ tut_12.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(){
    int a = 70;
    int* b = &a; // address of a;[&]
-   cout<<"The address of a is: "<<b<<endl;
+   cout<<"The address of a is: "<<&a<<endl;
+   cout<<"The address of a in b: "<<b<<endl;
+   cout<<"The value of b is: "<<*b<<endl;
 return 0;
 }
