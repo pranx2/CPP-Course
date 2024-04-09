dfcc4099baff15727e032e8537b2216bb5157db3
PRAN <prantokumar.net@gmail.com>
2024-04-09 21:20:34 +0600

    function

--- tut_15.cpp
+++ tut_15.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 
-int sum (int a, int b){
-   int c = a + b; 
-   return c;
-}
+// int sum (int a, int b){   ////function name (Arguments)
+//    int c = a + b; 
+//    return c;
+// }
+int sum (int a, int b);
 int main(){
    int num1, num2;
    cout<<"Enter first number: "<<endl;
@@ -12,6 +13,11 @@ int main(){
    cout<<"Enter second number: "<<endl;
    cin>>num2;
 
-   cout<<"The sum is : "<<sum(num1, num2);
+   cout<<"The sum is : "<<sum(num1, num2);  // function calling;
 return 0;
+}
+
+int sum (int a, int b){ // function initializing
+   int c = a + b; 
+   return c;
 }
