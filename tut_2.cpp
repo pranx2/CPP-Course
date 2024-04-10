4363677f396942382098d23d1218738457f65856
PRAN <prantokumar.net@gmail.com>
2024-04-10 22:58:33 +0600

    Variables

--- tut_2.cpp
+++ tut_2.cpp
@@ -10,7 +10,7 @@ using namespace std;
       a = 7 ;
       b = 8 ;
       double d; //big float numbers;
-      bool d; // true and false; 0,1;
+       bool d; // true and false; 0,1;
       cout<<"Total sum is: "<<a+b; 
-   return 0;
- }
+  return 0;
+}
