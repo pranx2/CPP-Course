d22ef135907891e2eecc58dc28cdb88a22b62695
PRAN <prantokumar.net@gmail.com>
2024-04-19 23:52:56 +0600

    oops classes example

--- tut_21.cpp
+++ tut_21.cpp
@@ -13,26 +13,26 @@ class Info{
 
       void setinfo(string  name, string  varsity, string  nationality, string  address,string  birthDate);
       void getinfo(){
-            cout<<"Employee Name: "          <<Name<<endl;
-            cout<<"Employee varsity: "       <<Varsity<<endl;
-            cout<<"Employee Nationality: "   <<Nationality<<endl;
-            cout<<"Employee Address: "       <<Address<<endl;
-            cout<<"Employee BirthDate: "     <<BirthDate<<endl;
-            cout<<"Employee Joining Date: "  <<joiningDate<<endl;
-            cout<<"Employee Salary: "        <<salary<<endl;
+            cout<<"Employee Name        : "<<Name       <<endl;
+            cout<<"Employee varsity     : "<<Varsity    <<endl;
+            cout<<"Employee Nationality : "<<Nationality<<endl;
+            cout<<"Employee Address     : "<<Address    <<endl;
+            cout<<"Employee BirthDate   : "<<BirthDate  <<endl;
+            cout<<"Employee Joining Date: "<<joiningDate<<endl;
+            cout<<"Employee Salary      : "<<salary     <<endl;
       }
 };
    void Info :: setinfo(string  name, string  varsity, string  nationality, string  address, string birthDate){
-         Name        = name;
-         Varsity       = varsity;
-         Nationality  = nationality;
-         Address       = address;
-         BirthDate     = birthDate;
+         Name       = name;
+         Varsity    = varsity;
+         Nationality= nationality;
+         Address    = address;
+         BirthDate  = birthDate;
    }
 int main(){
    Info pranto;
-   pranto.joiningDate= "01 january 2024";
-   pranto.salary= "12000 BDT";
+   pranto.joiningDate   = "01 january 2024";
+   pranto.salary  = "12000 BDT";
    pranto.setinfo("PRANTO", "Uttara University", "Bangladesh", "Mirpur-14", "16 December 2003");
    pranto.getinfo();
 return 0;
