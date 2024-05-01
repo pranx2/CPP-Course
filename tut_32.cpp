973ea17978fe90d8c680c0b897a6b7879124bb82
PRAN <prantokumar.net@gmail.com>
2024-05-01 13:00:26 +0600

    Default Arguments in class Construction

--- tut_32.cpp
+++ tut_32.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+class complex{
+   int data1;
+   int data2;
+      public :
+         void complexNumber(int a = 0, int b = 0){ //Default Arguments;
+            data1 = a ;
+            data2 = b ;
+         }
+         void printNumber();
+};
+void complex ::printNumber(){
+   cout<<"The data1 and data2 is "<<data1<<" and "<<data2<<endl;
+}
 int main(){
-
+   complex c,d;
+   c.complexNumber(); //call by reference; 
+   c.printNumber();
 return 0;
 }
