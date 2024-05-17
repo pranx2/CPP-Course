82f0c7def49fccd05fd12e71b21560ce52904d5a
PRAN <prantokumar.net@gmail.com>
2024-05-17 13:21:17 +0600

    Array of Objects Using Pointers

---
+++ tut_47.cpp
@@ -0,0 +1,48 @@
+/**
+ * A class to represent shop items with ID and price.
+ */
+#include<iostream>
+using namespace std;
+class shopItem{
+    int id;
+    float price;
+
+    public:
+        void setData(int a, float b){
+            id = a;
+            price = b;
+        }
+        void GetData(void){
+            cout<<"Item Id: "<<id<<endl;
+            cout<<"Item Price: "<<price<<endl;
+        }
+};
+ 
+int main() {
+    int size = 2;
+    int p, i;
+    float q;
+    
+    /**
+     * Assigns the value of a shopItem pointer to another pointer.
+     *
+     * @param ptrtemp Pointer to a shopItem object.
+     * @param ptr Pointer to a shopItem object to be assigned.
+     *
+     * @returns None
+     */
+     shopItem *ptr = new shopItem [size];
+    shopItem *ptrtemp = ptr; 
+for (i = 0; i < size; i++){
+    cout<<"Enter the ID and the Price: "<<i+1<<endl;
+    cin>>p>>q;
+    ptr->setData(p,q);
+    ptr++;
+}
+for(i = 0 ; i< size; i++) {
+    cout << "Item number : "<< i+1 <<endl;
+    ptrtemp->GetData();
+    ptrtemp++;
+}
+return 0;
+}
