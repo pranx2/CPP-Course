29b0ee9ab582ad28070fa973874ebe7010218143
PRAN <prantokumar.net@gmail.com>
2024-05-19 10:25:15 +0600

    bstract Base Class & Pure Virtual Functions

---
+++ tut_52.cpp
@@ -0,0 +1,26 @@
+/**
+ * This code includes the necessary standard C++ libraries for the program.
+ * It is recommended to include specific headers instead of the entire bits/stdc++.h header
+ * to reduce compilation time and avoid potential conflicts.
+ */
+#include<bits/stdc++.h>
+using namespace std;
+class MainBranch{
+    protected:
+        string Title_name;
+        float Rating;
+    public :
+        MainBranch(string t, float r){
+            Title_name = t;
+            Rating = r ;
+        }
+        /**
+         * Virtual function to display information.
+         * 
+         * This is a pure virtual function that must be implemented by derived classes.
+         * It serves as an interface for displaying information specific to each derived class.
+         */
+        virtual void display() = 0;
+};
+
+
