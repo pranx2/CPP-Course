b6ce969d0a7f822aa6d7f0ef4d50204d23415361
PRAN <prantokumar.net@gmail.com>
2024-05-24 16:12:17 +0600

    functional object

--- STL for CP/7.cpp
+++ STL for CP/7.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 int main(){
 int arr[] = {1,3,55, 22,500,34,2,45};
-sort(arr, arr+8,less<int>()); // sort the array in order...//greater means big to small..
-for (int i = 0; i < 8; i++)
-{
+sort(arr, arr+8,greater<int>()); // sort the array in order...//greater means big to small..
+for (int i = 0; i < 8; i++){
     cout<<arr[i]<<endl;
 }
 
