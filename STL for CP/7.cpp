0c99f5aeebcfc1bc54725086186e2b6c25ecfa2d
PRAN <prantokumar.net@gmail.com>
2024-05-24 15:23:05 +0600

    Functional Object

---
+++ STL for CP/7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[] = {1,3,55, 22,500,34,2,45};
+sort(arr, arr+8,less<int>()); // sort the array in order...//greater means big to small..
+for (int i = 0; i < 8; i++)
+{
+    cout<<arr[i]<<endl;
+}
+
+return 0;
+}
